ecdfdd646ad3168f891ce4a2690979affd2639d9
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-12-07 13:18:02 -0700

    Add suma...

--- src/test/java/calculadora/AppTest.java
+++ src/test/java/calculadora/AppTest.java
@@ -6,9 +6,22 @@ package calculadora;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// import org.junit.Before;
+
 public class AppTest {
+    Calculadora c;
+
+  //  @Before void setUp() {
+  //      c = new Calculadora();
+  //  }
+
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test public void testSuma() {
+        Calculadora c = new Calculadora();
+        assertTrue(c.suma(2,3)==5);
     }
 }
